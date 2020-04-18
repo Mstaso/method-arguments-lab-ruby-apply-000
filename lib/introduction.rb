a574830ed839ac4973b5467e51ad7a034dea4641
malcolm staso <mstaso1@gmail.com>
2020-04-18 16:45:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,3 @@
 def introduction_with_language (name, language)
-  puts "Hi, my name is #{name} and I am learning to program in the #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
